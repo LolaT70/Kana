9b19e027096ad5e5d6433f6db060a8d3d62a763a
LolaT70 <toajaller@outlook.com>
2023-02-11 10:04:46 +0100

    v0.71

---
+++ kana_cli.py
@@ -0,0 +1,5 @@
+import random
+kana=['a','i','u','e','o','ka','ki','ku','ke','ko','sa','shi','su'
+    ,'se','so','ta','chi','tsu','te','to','na','ni','nu','ne','no','ha','hi','fu','he','ho','ma','mi','mu','me','mo'
+          ,'ya','yu','yo','ra','ri','ru','re','ro','wa','wi','we','wo','n']
+print(kana[random.randint(0,47)])
